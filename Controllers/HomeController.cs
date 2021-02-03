63ce1115d055bf71ef87dc181575d2785a7e00b9
skbrockbank <70676703+skbrockbank@users.noreply.github.com>
2021-02-02 17:49:11 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using FilmCollection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddMovie(Movie newMovie)
+        {
+            if (ModelState.IsValid)
+            {
+                TempStorage.AddMovie(newMovie);
+                return View("Confirmation", newMovie);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult MyMovies()
+        {
+            return View(TempStorage.Movies);
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmCollection.Models
+{
+    public class Movie
+    {
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Year { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public string Rating { get; set; }
+        [Required]
+        public bool Edited { get; set; }
+        public string LentTo { get; set; }
+        [StringLength(25)]
+        public string Notes { get; set; }
+    }
+}
